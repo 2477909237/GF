4e43da64ea104d3ba826f9dd0208a812e9558549
刘帅斌 <2477909237@qq.com>
2022-11-05 13:35:40 +0800

    --story=导入LuBan配置工具

---
+++ Tool/ExportExcell/ExportConfig.bat
@@ -0,0 +1,25 @@
+set dotnet=dotnet
+set Luban.ClientServer.dll=..\LuBanTools\Luban.ClientServer\Luban.ClientServer.dll
+set define_date_dir=..\..\Common\Defines\__root__.xml
+set input_data_dir=..\..\Common\GameDatas
+set output_data_dir=..\..\Common\Export\Json
+set output_dataBytes_dir=..\..\Common\Export\Bytes
+set output_code_dir=..\..\GFProject\Assets\Plugins\LuBan\config
+set gen_types=code_cs_unity_bin,data_json
+
+
+%dotnet% %Luban.ClientServer.dll% -j cfg --^
+ -d %define_date_dir% ^
+ --input_data_dir %input_data_dir% ^
+ --output_data_dir %output_data_dir% ^
+ --output_code_dir %output_code_dir% ^
+ --gen_types %gen_types% ^
+ -s all
+ 
+ %dotnet% %Luban.ClientServer.dll% -j cfg --^
+ -d %define_date_dir% ^
+ --input_data_dir %input_data_dir% ^
+ --output_data_dir %output_dataBytes_dir% ^
+ --gen_types data_bin ^
+ -s all
+pause
