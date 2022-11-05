7fd92372adc1d7c4008c9e04fbd1f94cc4196d72
刘帅斌 <2477909237@qq.com>
2022-11-05 13:38:54 +0800

    --story=项目中的配置相关文件

---
+++ GFProject/Assets/GameFramework/Scripts/Runtime/DataTable/LuBanDataTableHelper.cs
@@ -0,0 +1,33 @@
+﻿using GameFramework.DataTable;
+
+namespace UnityGameFramework.Runtime
+{
+    public class LuBanDataTableHelper : DataTableHelperBase
+    {
+        public override bool ReadData(DataTableBase dataTable, string dataTableAssetName, object dataTableAsset, object userData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool ReadData(DataTableBase dataTable, string dataTableAssetName, byte[] dataTableBytes, int startIndex, int length,
+            object userData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool ParseData(DataTableBase dataTable, string dataTableString, object userData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool ParseData(DataTableBase dataTable, byte[] dataTableBytes, int startIndex, int length, object userData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ReleaseDataAsset(DataTableBase dataTable, object dataTableAsset)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
